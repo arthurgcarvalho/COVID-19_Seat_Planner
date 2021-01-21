b4b8c8effa7afa2ed357fabfcb73f9b6e96be159
arthurgcarvalho <arthur.carvalho@miamioh.edu>
2021-01-21 15:44:56 -0500

    fix plots

--- python/plot_results_group.py
+++ python/plot_results_group.py
@@ -20,10 +20,14 @@ def plot_layout_result(data_file):
     ## create matplotlib figure.
     fig, axs = plt.subplots()
     axs.set_aspect('equal')
-    axs.set_xlim((-5, max(seats[:,0]) + 10))
-    axs.set_ylim((-5, max(seats[:,1]) + 10))
+    axs.set_xlim((-5, max(seats[:,0]) + 5))
+    axs.set_ylim((-5, max(seats[:,1]) + 5))
     axs.invert_yaxis()
     axs.invert_xaxis()
+    axs.spines['top'].set_visible(False)
+    axs.spines['right'].set_visible(False)
+    axs.spines['bottom'].set_visible(False)
+    axs.spines['left'].set_visible(False)
 
     plt.tick_params(
         axis='both',  # changes apply to the x-axis
@@ -33,21 +37,22 @@ def plot_layout_result(data_file):
         labelbottom=False,  # labels along the bottom edge are off
         labelleft=False # labels along the left edge are off
     )
-    
+  
     for seat_index in range(len(seats)):    
-        rectangle = mpatch.Rectangle((seats[seat_index,0],seats[seat_index,1]), 5, 5, fill = False) #create squares centered at seat positions, length 5
-        axs.add_artist(rectangle, )
-        rx, ry = rectangle.get_xy()
-        cx = rx + rectangle.get_width()/2.0
-        cy = ry + rectangle.get_height()/2.0
-        if str(seats[seat_index,2]) != "-1":
-            axs.annotate(str(seats[seat_index,2]),  (cx, cy), color='b', weight='bold', fontsize=10, ha='center', va='center')
+        #rectangle = mpatch.Rectangle((seats[seat_index,0],seats[seat_index,1]), 5, 20, fill = False) #create squares centered at seat positions, length 5
+        #axs.add_artist(rectangle, )
+        #rx, ry = rectangle.get_xy()
+        #x = rx + rectangle.get_width()/2.0
+        #cy = ry + rectangle.get_height()/2.0
+        if int(seats[seat_index,2]) != -1:
+            axs.annotate(int(seats[seat_index,2]),  (seats[seat_index,0], seats[seat_index,1]), color='b', weight='bold', fontsize=10, ha='center', va='center')
         else:
-            axs.annotate('X',  (cx, cy), color='b', weight='bold', fontsize=10, ha='center', va='center')
+            axs.annotate('X', (seats[seat_index,0], seats[seat_index,1]), color='red', weight='bold', fontsize=10, ha='center', va='center')
     
     
     plt.savefig(filename + ".pdf", dpi=300)
 
 if __name__ == "__main__":
-    plot_layout_result("groups_output.xlsx")    
+    data_file = os.getcwd() + "\\python\\output\\file-1611255960626.xlsx" 
+    plot_layout_result(data_file)    
     
